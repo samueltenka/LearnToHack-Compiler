3065f510a1e649408f1b219d1398d6e4bfcc9a14
samueltenka <iguavadon@gmail.com>
2016-01-09 16:22:17 -0500

    ok, fixed parser output, tested. correctly computes 1 + 2 * 3 + 4 * ( 5 + 6 )    as    56. not only was there a problem w/ order of arguments to arithmetic expressions, it turns out: our algo. was wrong! need a stack to handle parenthesized expressions. this we made, using register 7 as our stack pointer.

--- SimpleExprParser.py
+++ SimpleExprParser.py
@@ -6,15 +6,35 @@ class Parser:
       return None if \
       self.pos>=len(self.toks) \
       else self.toks[self.pos]
+   def init_stack(self):
+       print('set 6 1.0')
+       print('set 7 50.0') # TODO: ensure beyond program
+   def pop_stack(self):
+       print('sub 6 7')
+       print('load 7 2')
+       print('sub 6 7')
+       print('load 7 1')
+       print('sub 6 7')
+       print('load 7 0')
+   def push_stack(self):
+       print('store 7 0')
+       print('add 6 7')
+       print('store 7 1')
+       print('add 6 7')
+       print('store 7 2')
+       print('add 6 7')
    def rec(self, tok):
       assert(self.peek()==tok)
       self.pos += 1
    def rec_atom(self):
       if self.peek() == '(':
+         self.push_stack()
          self.rec('(')
          self.rec_expr()
          self.rec(')')
-         print('copy 2 0')
+         print('copy 0 3')
+         self.pop_stack()
+         print('copy 3 0')
       else:
          num = self.peek()
          print('set 0 '+num)
@@ -25,14 +45,17 @@ class Parser:
          self.rec('*')
          print('copy 0 1')
          self.rec_atom()
-         print('mul 0 1')
+         print('mul 1 0')
    def rec_expr(self):
       self.rec_term()
       while self.peek() == '+':
          self.rec('+')
          print('copy 0 2')
          self.rec_term()
-         print('add 0 2')
+         print('add 2 0')
+   def rec_program(self):
+       self.init_stack()
+       self.rec_expr()
 
 P = Parser('1.0 + 2.0 * 3.0 + 4.0 * ( 5.0 + 6.0 )')
-P.rec_expr()
+P.rec_program()
